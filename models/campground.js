55d02a36f2e388c029f9614f59f8a00479e39487
asim delvi <asimdelvi30@gmail.com>
2022-06-18 17:25:25 +0530

    add cluster map on index.js

--- models/campground.js
+++ models/campground.js
@@ -11,34 +11,45 @@ ImageSchema.virtual("thumbnail").get(function () {
   return this.url.replace("/upload", "/upload/w_200");
 });
 
-const CampgroundSchema = new Schema({
-  title: String,
-  images: [ImageSchema],
-  // its specified as geoJson format, will be found in mongoose docs.
-  geometry: {
-    type: {
-      type: String,
-      enum: ["Point"],
-      required: true,
+// By default mongoose does not include virtual when you convert a document to JSON.
+// So we have to use...
+const opts = { toJSON: { virtuals: true } };
+
+const CampgroundSchema = new Schema(
+  {
+    title: String,
+    images: [ImageSchema],
+    // its specified as geoJson format, will be found in mongoose docs.
+    geometry: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-    coordinates: {
-      type: [Number],
-      required: true,
+    description: String,
+    price: Number,
+    location: String,
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
+    reviews: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
   },
-  description: String,
-  price: Number,
-  location: String,
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  reviews: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Review",
-    },
-  ],
+  opts
+);
+
+CampgroundSchema.virtual("properties.popUpText").get(function () {
+  return `<a href="/campgrounds/${this._id}">${this.title}</a>`;
 });
 
 // particular deleted document will be sent as an argument to the post middleware.
